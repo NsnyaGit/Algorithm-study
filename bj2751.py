f7273e1095db11031e3c8f85c7da5058e88bbae7
JunPyo Jeon <nsnyagit@gmail.com>
2024-01-09 17:36:07 +0900

    20240109

--- bj2751.py
+++ bj2751.py
@@ -4,8 +4,10 @@ N = int(sys.stdin.readline())
 num_list = []
 
 while N >0 :
-    num_list.append(sys.stdin.readline().rstrip())
+    num_list.append(int(sys.stdin.readline()))
     N-=1
 
 num_list.sort()
+
+num_list = list(map(str,num_list))
 print("\n".join(num_list))
