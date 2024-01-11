a263ba281708b756a23583c474ab6fc3a60288a3
JunPyo Jeon <nsnyagit@gmail.com>
2024-01-11 18:06:59 +0900

    20240111

--- bj1676_factorial.py
+++ bj1676_factorial.py
@@ -1,2 +1,13 @@
 #N!の結果で、後ろから初めて０じゃない数字が出るまでの０が何個あるかを教えてくれるプログラム作成
 
+import sys
+
+def factorial(n):
+    ret = 1
+    while n > 0:
+        ret *= n
+        n-=1
+    
+
+
+n = int(sys.stdin.readline())
