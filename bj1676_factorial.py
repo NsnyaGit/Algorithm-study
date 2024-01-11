fa1f6c084d148c634da73bb6851b67f069123acf
JunPyo Jeon <nsnyagit@gmail.com>
2024-01-11 18:29:15 +0900

    20240111

--- bj1676_factorial.py
+++ bj1676_factorial.py
@@ -1,13 +1,26 @@
 #N!の結果で、後ろから初めて０じゃない数字が出るまでの０が何個あるかを教えてくれるプログラム作成
 
-import sys
-
 def factorial(n):
     ret = 1
     while n > 0:
         ret *= n
         n-=1
-    
+    return ret
 
+import sys
 
+cnt = 0
 n = int(sys.stdin.readline())
+n = str(factorial(n))
+
+for i in range(1, len(n)+1):
+    if n[-i] != '0':
+        break
+    else:
+        cnt+=1
+        
+print(cnt)
+
+# 다른 답
+# n = int(sys.stdin.readline())
+# print(n//5 + n//25 + n//125)
