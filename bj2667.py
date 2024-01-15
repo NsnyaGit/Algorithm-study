f02fbd9f7da1bf6ab093afdc8b4cc02448ef9111
JunPyo Jeon <nsnyagit@gmail.com>
2024-01-15 20:49:01 +0900

    20240115

--- bj2667.py
+++ bj2667.py
@@ -1,6 +1,12 @@
 #https://www.acmicpc.net/problem/2667
 
+from collections import deque
 import sys
 input = sys.stdin.readline
 
-N = int(input())
+
+def bfs(graph,x,y):
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+
+    
