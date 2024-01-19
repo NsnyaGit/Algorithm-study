444528e8407098d47fa14c687e918e1e65901f81
JunPyo Jeon <nsnyagit@gmail.com>
2024-01-19 22:09:25 +0900

    20240119

--- bj1141.py
+++ bj1141.py
@@ -3,4 +3,15 @@ import sys
 n = int(sys.stdin.readline())
 
 
-nk = 
+lst = []
+ans = []
+for i in range(n):
+    num = sys.stdin.readline().rstrip("\n")
+    lst.append(num)
+
+lst.sort(reverse=True)
+for i in range(len(lst)-1):
+    for j in range(len(lst[i+1])):
+        if lst[i][j] != lst[i+1][j]:
+            break
+        
