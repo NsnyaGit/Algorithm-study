ededdfd6709cb9ec1acd69fe66dde9b49eb158b5
JunPyo Jeon <nsnyagit@gmail.com>
2024-01-23 00:12:04 +0900

    20240123

--- bj2504.py
+++ bj2504.py
@@ -6,12 +6,26 @@ from collections import deque
 
 sign = sys.stdin.readline().rstrip("\n")
 stack = deque()
-
-
+check_val = 0
+is_valid = 1
 #올바른 괄호열인지 판단
-
 for i in sign:
     if i == '(' or i == '[':
-        stack.append()
+        stack.append(i)
     else:
-        
+        if len(stack) == 0:
+            is_valid = 0
+            break
+        check_val = stack.pop()
+        if i == ')':
+            if check_val != '(':
+                is_valid = 0
+                break
+        elif i == ']':
+            if check_val != '[':
+                is_valid = 0
+                break
+if len(stack) != 0:
+    is_valid = 0
+
+#괄호열 계산
