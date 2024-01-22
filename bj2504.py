e22f31b47e2b07fb6072f592e5fc1b645bade8a1
JunPyo Jeon <nsnyagit@gmail.com>
2024-01-22 23:29:08 +0900

    20240122

--- bj2504.py
+++ bj2504.py
@@ -1,1 +1,14 @@
 #https://www.acmicpc.net/problem/2504
+
+import sys
+from collections import deque
+
+
+sign = sys.stdin.readline().rstrip("\n")
+stack = deque()
+
+
+#올바른 괄호열인지 판단
+
+for i in sign:
+    print(i)
