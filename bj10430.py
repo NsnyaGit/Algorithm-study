89af9220b56ca93e127cf45302b0146417351edc
JunPyo Jeon <nsnyagit@gmail.com>
2024-01-27 02:19:23 +0900

    20240127

--- bj10430.py
+++ bj10430.py
@@ -2,3 +2,10 @@
 #(A×B)%Cは ((A%C) × (B%C))%C と等しいだろうか
 #A, B, Cが与えられた時, 上の四つのケースの答えを求めよう
 
+a,b,c = map(int,input().split())
+
+print((a+b)%c)
+print(((a%c) + (b%c))%c)
+print((a*b)%c)
+print(((a%c) * (b%c))%c)
+       
